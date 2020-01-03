983e47c8a52228ab82699149da0667f9c2fe15dc
longyuan02 <longyuan02@qq.com>
2020-01-03 16:58:23 +0800

    更改请求

--- js/utils/API.js
+++ js/utils/API.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 let ApiList = {
-  Hosts:'https://cms-uat.gomegold.com/',
-  cmsUrl:'https://m-uat.gomegold.com/',
-  H5Url:'https://m-uat.gomegold.com/',
+  Hosts:'https://cms.gomegold.com/',
+  cmsUrl:'https://m.gomegold.com/',
+  H5Url:'https://m.gomegold.com/',
 }
 export default ApiList;
